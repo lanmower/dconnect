a90e747a23a5fc694cb39eb59eb987fc3eba6029
Glitch (dconnect-server) <none>
2019-02-12 19:16:36 +0000

    dconnect webserver from glitch

--- src/services/index.js
+++
@@ -1,8 +0,0 @@
-const steem = require('steem');
-const getMessagesService = require('./messages/messages.service.js');
-const users = require('./users/users.service.js');
-
-module.exports = function (app) {
-  app.getMessagesService = getMessagesService;
-  app.configure(users);
-};
