d94189ac5ab60119ebad13fbf7e6a93a5eaa13c9
lanmower <almagestfraternite@gmail.com>
2019-04-10 18:07:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 screen -dmS ipfs bash -c 'while true; do ./ipfs daemon && break; done'
-screen -dmS ipfs bash -c 'while true; do node server.js && break; done'
+screen -dmS dconnect bash -c 'while true; do node server.js && break; done'
